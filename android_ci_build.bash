bcf9c199c9ca40237588cc3aa471651020c51a27
aaaa派 <89151134+aaaapai@users.noreply.github.com>
2025-03-23 11:48:39 +0800

    Update android_ci_build.bash

--- android_ci_build.bash
+++ android_ci_build.bash
@@ -3,8 +3,8 @@
 
 cmake_build () {
   ANDROID_ABI=$1
-  mkdir -p build-$ANDROID_ABI
-  cd build-$ANDROID_ABI
+  mkdir -p build
+  cd build
   cmake $GITHUB_WORKSPACE/src/main/cpp -DANDROID_PLATFORM=24 -DANDROID_ABI=$ANDROID_ABI -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK_LATEST_HOME/build/cmake/android.toolchain.cmake
   cmake --build . --config Release --parallel 4
 }
