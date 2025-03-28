04d569afdc9aee425c0a63f4086d845b0620f862
aaaa派 <89151134+aaaapai@users.noreply.github.com>
2025-03-29 00:06:06 +0800

    Update buffer.h

--- src/main/cpp/gl/dsa/buffer.h
+++ src/main/cpp/gl/dsa/buffer.h
@@ -15,21 +15,21 @@
 extern "C" {
 #endif
 
-GLAPI void APIENTRY glNamedBufferSubData (GLuint buffer, GLintptr offset, GLsizeiptr size, const void *data);
+GLAPI APIENTRY void glNamedBufferSubData (GLuint buffer, GLintptr offset, GLsizeiptr size, const void *data);
 
-GLAPI GLAPIENTRY void glCreateBuffers(GLsizei n, GLuint* buffers);
+GLAPI APIENTRY void glCreateBuffers(GLsizei n, GLuint* buffers);
 
-GLAPI GLAPIENTRY void glFlushMappedNamedBufferRange(GLuint buffer, GLintptr offset, GLsizeiptr length);
+GLAPI APIENTRY void glFlushMappedNamedBufferRange(GLuint buffer, GLintptr offset, GLsizeiptr length);
 
-GLAPI GLAPIENTRY void glGetNamedBufferParameteriv(GLuint buffer, GLenum pname, GLint* params);
+GLAPI APIENTRY void glGetNamedBufferParameteriv(GLuint buffer, GLenum pname, GLint* params);
 
-GLAPI GLAPIENTRY void glGetNamedBufferParameteri64v(GLuint buffer, GLenum pname, GLint64* params);
+GLAPI APIENTRY void glGetNamedBufferParameteri64v(GLuint buffer, GLenum pname, GLint64* params);
 
-GLAPI GLAPIENTRY void glGetNamedBufferPointerv(GLuint buffer, GLenum pname, void* *params);
+GLAPI APIENTRY void glGetNamedBufferPointerv(GLuint buffer, GLenum pname, void* *params);
 
-GLAPI GLAPIENTRY void glGetNamedBufferSubData(GLuint buffer, GLintptr offset, GLsizeiptr size, void* data);
+GLAPI APIENTRY void glGetNamedBufferSubData(GLuint buffer, GLintptr offset, GLsizeiptr size, void* data);
 
-GLAPI GLAPIENTRY void* glMapNamedBufferRange(GLuint buffer, GLintptr offset, GLsizeiptr length, GLbitfield access);
+GLAPI APIENTRY void* glMapNamedBufferRange(GLuint buffer, GLintptr offset, GLsizeiptr length, GLbitfield access);
 
 #ifdef __cplusplus
 }
