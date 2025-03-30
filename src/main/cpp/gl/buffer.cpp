66cac313b867c53a3f98d1ca0633ddf382ac4fb0
aaaa派 <89151134+aaaapai@users.noreply.github.com>
2025-03-30 21:31:57 +0800

    Update buffer.cpp

--- src/main/cpp/gl/buffer.cpp
+++ src/main/cpp/gl/buffer.cpp
@@ -146,6 +146,10 @@ void glGenBuffers(GLsizei n, GLuint *buffers) {
     }
 }
 
+void glGenBuffersARB(GLsizei n, GLuint *buffers) {
+    glGenBuffers(n, buffers);
+}
+
 void glDeleteBuffers(GLsizei n, const GLuint *buffers) {
     LOG()
     LOG_D("glDeleteBuffers(%i, %p)", n, buffers)
@@ -159,10 +163,18 @@ void glDeleteBuffers(GLsizei n, const GLuint *buffers) {
     }
 }
 
+void glDeleteBuffersARB(GLsizei n, const GLuint *buffers) {
+    glDeleteBuffers(n, buffers);
+}
+
 GLboolean glIsBuffer(GLuint buffer) {
     LOG()
     LOG_D("glIsBuffer, buffer = %d", buffer)
     return has_buffer(buffer);
+}
+
+GLboolean glIsBufferARB(GLuint buffer) {
+    glIsBuffer(buffer);
 }
 
 void glBindBuffer(GLenum target, GLuint buffer) {
