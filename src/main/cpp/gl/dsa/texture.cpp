3bdde62781f775ab6669618ebc468851a7e9b7ef
aaaa派 <89151134+aaaapai@users.noreply.github.com>
2025-05-06 23:27:48 +0800

    Update texture.cpp

--- src/main/cpp/gl/dsa/texture.cpp
+++ src/main/cpp/gl/dsa/texture.cpp
@@ -1,1 +1,9 @@
+#include "texture.h"
+#include "../texture.h"
 
+#define DEBUG 0
+
+void glCreateTextures(GLenum target, GLsizei n, GLuint *textures) {
+     glBindTexture(target, textures);
+     glGenTextures(n, textures);
+}
