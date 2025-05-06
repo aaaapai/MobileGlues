7bd69dc84ca7e4a546f790f4064549832f572f6c
aaaa派 <89151134+aaaapai@users.noreply.github.com>
2025-05-06 23:29:19 +0800

    Update texture.h

--- src/main/cpp/gl/dsa/texture.h
+++ src/main/cpp/gl/dsa/texture.h
@@ -1,1 +1,20 @@
+#ifndef MOBILEGLUES_DSA_TEXTURE_H
+#define MOBILEGLUES_DSA_TEXTURE_H
 
+#include <vector>
+
+#include "GL/gl.h"
+#include "../log.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+GLAPI void APIENTRY glCreateTextures(GLenum target, GLsizei n, GLuint *textures);
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif //MOBILEGLUES_DSA_TEXTURE_H
