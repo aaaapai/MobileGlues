7fa062f5c5fd484422d7866695c05cfa72cfd744
aaaa派 <89151134+aaaapai@users.noreply.github.com>
2025-07-06 21:34:40 +0800

    Update multidraw.h

--- src/main/cpp/gl/multidraw.h
+++ src/main/cpp/gl/multidraw.h
@@ -10,8 +10,8 @@
 #include <cstring>
 #include <GLES3/gl3.h>
 #include "../includes.h"
-#include "gl.h"
-#include "glcorearb.h"
+#include <GL/gl.h>
+#include <GL/glcorearb.h>
 #include "log.h"
 #include "../gles/loader.h"
 #include "mg.h"
