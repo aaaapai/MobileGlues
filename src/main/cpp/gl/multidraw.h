872643825bf24e829cc0dbe3eab7670c014562fe
aaaa派 <89151134+aaaapai@users.noreply.github.com>
2025-07-06 21:32:18 +0800

    Update multidraw.h

--- src/main/cpp/gl/multidraw.h
+++ src/main/cpp/gl/multidraw.h
@@ -10,11 +10,15 @@
 #include <cstring>
 #include <GLES3/gl3.h>
 #include "../includes.h"
-#include "GL/gl.h"
-#include "GL/glcorearb.h"
+#include "gl.h"
+#include "glcorearb.h"
 #include "log.h"
 #include "../gles/loader.h"
 #include "mg.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 struct draw_elements_indirect_command_t {
     GLuint  count;
@@ -29,23 +33,6 @@ struct drawcmd_compute_t {
     GLint   baseVertex;
 };
 
-
-typedef struct {
-    bool ready;
-    GLuint indirectRenderBuffer;
-} ltw_basevertex_renderer_t;
-typedef struct {
-    GLuint count;
-    GLuint instanceCount;
-    GLuint firstIndex;
-    GLint baseVertex;
-    GLuint reservedMustBeZero;
-} indirect_pass_t;
-GLint type_bytes(GLenum type);
-GLuint multidraw_element_buffer;
-#define MAX_BOUND_BUFFERS 9
-GLuint bound_buffers[MAX_BOUND_BUFFERS];
-
 GLAPI GLAPIENTRY void glMultiDrawElementsBaseVertex(GLenum mode, GLsizei *counts, GLenum type, const void *const *indices, GLsizei primcount, const GLint *basevertex);
 GLAPI GLAPIENTRY void mg_glMultiDrawElementsBaseVertex_indirect(GLenum mode, GLsizei *counts, GLenum type, const void *const *indices, GLsizei primcount, const GLint *basevertex);
 GLAPI GLAPIENTRY void mg_glMultiDrawElementsBaseVertex_multiindirect(GLenum mode, GLsizei *counts, GLenum type, const void *const *indices, GLsizei primcount, const GLint *basevertex);
@@ -59,6 +46,9 @@ GLAPI GLAPIENTRY void mg_glMultiDrawElements_multiindirect(GLenum mode, const GL
 GLAPI GLAPIENTRY void mg_glMultiDrawElements_basevertex(GLenum mode, const GLsizei *count, GLenum type, const void *const *indices, GLsizei primcount);
 GLAPI GLAPIENTRY void mg_glMultiDrawElements_drawelements(GLenum mode, const GLsizei *count, GLenum type, const void *const *indices, GLsizei primcount);
 GLAPI GLAPIENTRY void mg_glMultiDrawElements_compute(GLenum mode, const GLsizei *count, GLenum type, const void *const *indices, GLsizei primcount);
-GLAPI GLAPIENTRY void mg_glMultiDrawElements_ltw(GLenum mode, const GLsizei *count, GLenum type, const void *const *indices, GLsizei primcount);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif //MOBILEGLUES_MULTIDRAW_H
