9380bed6d4fd67fe04508fa9bfbb20073f4902cf
aaaa派 <89151134+aaaapai@users.noreply.github.com>
2025-07-07 22:43:52 +0800

    Update glsl_for_es.cpp

--- src/main/cpp/gl/glsl/glsl_for_es.cpp
+++ src/main/cpp/gl/glsl/glsl_for_es.cpp
@@ -624,7 +624,7 @@ std::vector<unsigned int> glsl_to_spirv(GLenum shader_type, int glsl_version, co
     }
 
     shaderc_compile_options_t opts = shaderc_compile_options_initialize();
-    shaderc_compile_options_set_forced_version_profile(opts, 330, shaderc_profile_core);
+    shaderc_compile_options_set_forced_version_profile(opts, 450, shaderc_profile_core);
     shaderc_compile_options_set_auto_map_locations(opts, true);
     shaderc_compile_options_set_auto_bind_uniforms(opts, true);
 
