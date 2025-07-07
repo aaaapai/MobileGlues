ddbe72297d7f6dfe9734821ddc26ffd459864b4c
aaaa派 <89151134+aaaapai@users.noreply.github.com>
2025-07-07 11:31:15 +0800

    Update glsl_for_es.cpp

--- src/main/cpp/gl/glsl/glsl_for_es.cpp
+++ src/main/cpp/gl/glsl/glsl_for_es.cpp
@@ -574,6 +574,8 @@ std::string preprocess_glsl(const std::string& glsl, GLenum glsl_type) {
     replace_all(ret, "#version 140", "#version 330");
     replace_all(ret, "#version 150", "#version 330");
 
+    replace_all(ret, "noperspective ", "");//哥们的手机就是飞舞
+
     // Replace deprecated syntax
     if (glsl_type == GL_VERTEX_SHADER) {
         replace_all(ret, "attribute", "in");
