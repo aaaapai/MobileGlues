4cae0857f45b89bc3c9ef27718df129a141e1c06
aaaapai <89151134+aaaapai@users.noreply.github.com>
2025-07-14 19:05:17 +0800

    Update main.cpp

--- src/main/cpp/main.cpp
+++ src/main/cpp/main.cpp
@@ -225,25 +225,23 @@ void proc_init() {
     init_settings();
 
     load_libs();
-    // init_target_egl();
+    init_target_egl();
     init_target_gles();
 
     init_settings_post();
 
 #ifndef __APPLE__
     init_libshaderconv();
-    // init_watermark_res();
+    init_watermark_res();
 #endif
     
 #if PROFILING
     init_perfetto();
 #endif
 
-/*
 // Cleanup
 #ifndef __APPLE__
     destroy_temp_egl_ctx();
 #endif
-*/
     g_initialized = 1;
 }
