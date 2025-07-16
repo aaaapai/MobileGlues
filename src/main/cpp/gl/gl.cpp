23909f03af1841670341bb14c7777f96ace2678e
aaaapai <89151134+aaaapai@users.noreply.github.com>
2025-07-16 18:20:01 +0800

    Update gl.cpp

--- src/main/cpp/gl/gl.cpp
+++ src/main/cpp/gl/gl.cpp
@@ -108,6 +108,25 @@ void DrawDepthClearTri() {
     GLES.glColorMask(prevColorMask[0], prevColorMask[1], prevColorMask[2], prevColorMask[3]);
 }
 
+void glClear(GLbitfield mask) {
+    LOG();
+    LOG_D("glClear, mask = 0x%x", mask);
+
+    if (global_settings.angle_depth_clear_fix_mode == AngleDepthClearFixMode::Mode1 &&
+        global_settings.angle == AngleMode::Enabled && 
+        mask == GL_DEPTH_BUFFER_BIT && 
+        fabs(currentDepthValue - 1.0f) <= 0.001f) {
+        // Workaround for ANGLE depth-clear bug: if depth≈1.0, draw a fullscreen triangle at z=1.0 to force actual depth buffer write.
+        DrawDepthClearTri();
+        // Clear again
+        GLES.glClear(mask);
+    } else {
+        GLES.glClear(mask);
+    }
+
+    CHECK_GL_ERROR;
+}
+
 void glHint(GLenum target, GLenum mode) {
     LOG()
     LOG_D("glHint, target = %s, mode = %s", glEnumToString(target), glEnumToString(mode))
