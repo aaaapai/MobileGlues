0a5c853afbb6cc7f652788e4206b21ff3d4bbfd9
aaaapai <89151134+aaaapai@users.noreply.github.com>
2025-07-17 17:48:27 +0800

    Update loader.h

--- src/main/cpp/gles/loader.h
+++ src/main/cpp/gles/loader.h
@@ -148,6 +148,7 @@ struct gles_caps_t {
     int GL_EXT_buffer_storage;
     int GL_EXT_disjoint_timer_query;
     int GL_QCOM_texture_lod_bias;
+    int GL_EXT_texture_lod_bias;
     [[maybe_unused]] int GL_EXT_blend_func_extended;
     [[maybe_unused]] int GL_EXT_texture_format_BGRA8888;
     [[maybe_unused]] int GL_EXT_read_format_bgra;
