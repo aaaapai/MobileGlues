593c66e7ed1498febf78e138a4a2d8e04bdb362d
aaaapai <89151134+aaaapai@users.noreply.github.com>
2025-07-18 00:08:31 +0800

    Update texture.cpp

--- src/main/cpp/gl/texture.cpp
+++ src/main/cpp/gl/texture.cpp
@@ -1275,6 +1275,7 @@ void glBindSamplers(GLuint first, GLsizei count, const GLuint* samplers) {
     CHECK_GL_ERROR;
 } //DeepSeek
 
+/*
 void glTextureSubImage2D(GLuint texture, GLint level, GLint xoffset, GLint yoffset, 
                         GLsizei width, GLsizei height, GLenum format, 
                         GLenum type, const void *pixels) {
@@ -1292,3 +1293,4 @@ void glTextureSubImage2D(GLuint texture, GLint level, GLint xoffset, GLint yoffs
     // 恢复之前绑定的纹理
     GLES.glBindTexture(GL_TEXTURE_2D, (GLuint)prevTexture);
 }
+*/
