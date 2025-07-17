67998c64b8a8832e18e69b3372ea2946fc535800
aaaapai <89151134+aaaapai@users.noreply.github.com>
2025-07-17 19:44:57 +0800

    Update loader.h

--- src/main/cpp/gles/loader.h
+++ src/main/cpp/gles/loader.h
@@ -141,6 +141,7 @@ extern "C" GLAPI GLAPIENTRY type name(__VA_ARGS__) { \
     LOG_W("Stub function: %s @ %s(...)", RENDERERNAME, __FUNCTION__);       \
 }
 
+#undef GL_EXT_texture_lod_bias
 struct gles_caps_t {
     int maxtex;
     int major;
@@ -162,6 +163,7 @@ struct gles_caps_t {
     int GL_EXT_texture_rg;
     int GL_EXT_texture_query_lod;
 };
+#define GL_EXT_texture_lod_bias 1
 
 extern struct gles_caps_t g_gles_caps;
 
