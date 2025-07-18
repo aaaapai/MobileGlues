e618f39defbf6d1406c9bf53b285b7a4b9b31c78
aaaapai <89151134+aaaapai@users.noreply.github.com>
2025-07-18 17:12:34 +0800

    Update framebuffer.h

--- src/main/cpp/gl/dsa/framebuffer.h
+++ src/main/cpp/gl/dsa/framebuffer.h
@@ -15,6 +15,23 @@
 extern "C" {
 #endif
 
+struct attachment_t {
+    GLenum textarget;
+    GLuint texture;
+    GLint level;
+};
+
+struct framebuffer_t {
+    GLenum current_target;
+    struct attachment_t* draw_attachment;
+    struct attachment_t* read_attachment;
+};
+
+struct framebuffer_t* bound_framebuffer;
+extern GLint MAX_DRAW_BUFFERS;
+void rebind_framebuffer(GLenum old_attachment, GLenum target_attachment);
+GLint getMaxDrawBuffers();
+
 GLAPI GLAPIENTRY void glCreateFramebuffers(GLsizei n, GLuint* framebuffers);
 
 GLAPI GLAPIENTRY void glNamedFramebufferRenderbuffer(GLuint framebuffer, GLenum attachment, GLenum renderbuffertarget, GLuint renderbuffer);
