be7f90bc732ce78c90ec4db23f443a0a19f36e16
aaaapai <89151134+aaaapai@users.noreply.github.com>
2025-07-18 08:30:29 +0800

    Update getter.cpp

--- src/main/cpp/gl/getter.cpp
+++ src/main/cpp/gl/getter.cpp
@@ -388,6 +388,36 @@ const GLubyte * glGetStringi(GLenum name, GLuint index) {
     return nullptr;
 }
 
+void glQueryCounter(GLuint id, GLenum target) {
+    LOG()
+
+    GLES.glQueryCounterEXT(id, target);
+}
+
+void glGetQueryObjectui64v(GLuint id, GLenum pname, GLuint64 *params) {
+
+    LOG()
+    LOG_D("Entering glGetQueryObjectui64v(id=%u, pname=0x%X, params=%p)", id, pname, params);
+
+    if (!params) {
+        LOG_W("Warning: glGetQueryObjectui64v called with NULL params pointer");
+        return;
+    }
+
+    // GLES3 implementation - may require extension checks
+    if (pname == GL_QUERY_RESULT || pname == GL_QUERY_RESULT_AVAILABLE) {
+        GLES.glGetQueryObjectuivEXT(id, pname, (GLuint*)params); // Note: potential precision loss
+    } else if (pname == GL_QUERY_RESULT_NO_WAIT) {
+        // Check if extension is available
+            GLES.glGetQueryObjectuivEXT(id, pname, (GLuint*)params);
+    }
+
+    GLenum err;
+    while ((err = glGetError()) != GL_NO_ERROR) {
+        LOG_E("OpenGL error in glGetQueryObjectui64v: 0x%X", err);
+    }
+}
+
 void glGetQueryObjectiv(GLuint id, GLenum pname, GLint* params) {
     LOG()
     GLES.glGetQueryObjectivEXT(id, pname, params);
