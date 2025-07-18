2baf9b5511a3f2444d9adedd81ea5b884de8fef4
aaaapai <89151134+aaaapai@users.noreply.github.com>
2025-07-18 08:44:19 +0800

    Update buffer.h

--- src/main/cpp/gl/buffer.h
+++ src/main/cpp/gl/buffer.h
@@ -100,14 +100,6 @@ GLAPI GLAPIENTRY GLboolean glIsVertexArray(GLuint array);
 
 GLAPI GLAPIENTRY void glBindVertexArray(GLuint array);
 
-GLAPI GLAPIENTRY void glClearBufferData(GLenum target, GLenum internalformat, GLenum format, GLenum type, const void *data);
-
-GLAPI GLAPIENTRY void glClearNamedBufferData(GLuint buffer, GLenum internalformat, GLenum format, GLenum type, const void *data);
-
-GLAPI GLAPIENTRY void glClearBufferSubData (GLenum target, GLenum internalformat, GLintptr offset, GLsizeiptr size, GLenum format, GLenum type, const void *data);
-
-GLAPI GLAPIENTRY void glClearNamedBufferSubData (GLuint buffer, GLenum internalformat, GLintptr offset, GLsizeiptr size, GLenum format, GLenum type, const void *data);
-
 #ifdef __cplusplus
 }
 #endif
