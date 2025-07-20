602a689f0abe5bc554f359d0fc045a2e7f968a4f
aaaapai <89151134+aaaapai@users.noreply.github.com>
2025-07-20 17:09:06 +0800

    Update framebuffer.cpp

--- src/main/cpp/gl/dsa/framebuffer.cpp
+++ src/main/cpp/gl/dsa/framebuffer.cpp
@@ -6,10 +6,12 @@
 
 #include "../log.h"
 #include "../config/settings.h"
+#include <ankerl/unordered_dense.h>
 
 #define DEBUG 1
 
 extern GLint MAX_DRAW_BUFFERS;
+extern ankerl::unordered_dense::map<GLuint, GLenum> g_textureTargetMap;
 extern void rebind_framebuffer(GLenum old_attachment, GLenum target_attachment);
 extern GLint getMaxDrawBuffers();
 
