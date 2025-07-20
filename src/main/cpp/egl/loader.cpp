63d6e1a380ac4ad0bcfe27302e34868f821fc0e8
aaaapai <89151134+aaaapai@users.noreply.github.com>
2025-07-20 16:54:26 +0800

    Update loader.cpp

--- src/main/cpp/egl/loader.cpp
+++ src/main/cpp/egl/loader.cpp
@@ -52,7 +52,7 @@ void init_target_egl(void) {
             EGL_NONE
     };
 
-    EGLint ctxAttribs[] = { EGL_CONTEXT_CLIENT_VERSION, 3, EGL_NONE };
+    EGLint ctxAttribs[] = { EGL_CONTEXT_CLIENT_VERSION, 3, EGL_CONTEXT_MINOR_VERSION, 2, EGL_NONE };
 
     EGLint pbAttribs[] = { EGL_WIDTH, 32, EGL_HEIGHT, 32, EGL_NONE };
 
