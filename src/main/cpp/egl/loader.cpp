d79b7e0c9b280f3ab61c34256ba78068b5863b93
aaaapai <89151134+aaaapai@users.noreply.github.com>
2025-07-20 12:28:02 +0800

    Update loader.cpp

--- src/main/cpp/egl/loader.cpp
+++ src/main/cpp/egl/loader.cpp
@@ -85,7 +85,7 @@ void init_target_egl(void) {
 
     if (configsFound == 0) {
         configAttribs[6] = 0;
-        ChooseConfig_result = egl_eglChooseConfig(eglDisplay, configAttribs, &pbufConfig, 1, &configsFound):
+        ChooseConfig_result = egl_eglChooseConfig(eglDisplay, configAttribs, &pbufConfig, 1, &configsFound);
         if (ChooseConfig_result != EGL_TRUE) {
             LOG_E("Retry eglChooseConfig failed (0x%x)", egl_eglGetError());
             goto cleanup;
