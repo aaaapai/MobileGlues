93b4a94a6064455b18768fe2b7ade537f2d2c52c
aaaapai <89151134+aaaapai@users.noreply.github.com>
2025-07-23 19:42:06 +0800

    Update gl.cpp

--- src/main/cpp/gl/gl.cpp
+++ src/main/cpp/gl/gl.cpp
@@ -119,14 +119,6 @@ void DrawDepthClearTri() {
     GLES.glColorMask(prevColorMask[0], prevColorMask[1], prevColorMask[2], prevColorMask[3]);
 }
 
-#if defined(__aarch64__)
-#define GLES_MEMORY_BARRIER() \
-    do { \
-        __asm__ __volatile__("dmb ishst" ::: "memory"); \
-    } while(0)
-#else
-#define GLES_MEMORY_BARRIER() ((void)0)
-#endif
 void glClear(GLbitfield mask) {
     LOG()
     LOG_D("glClear, mask = 0x%x", mask)
@@ -145,7 +137,6 @@ void glClear(GLbitfield mask) {
         // Clear again
         GLES.glClear(mask);
     } else {*/
-        GLES_MEMORY_BARRIER();
         GLES.glClear(mask);
     
     //}
