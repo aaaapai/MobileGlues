086eb942cd1cbb9fc3cd1df12571c4f528882749
aaaapai <89151134+aaaapai@users.noreply.github.com>
2025-07-24 17:10:22 +0800

    Update framebuffer.cpp

--- src/main/cpp/gl/dsa/framebuffer.cpp
+++ src/main/cpp/gl/dsa/framebuffer.cpp
@@ -108,19 +108,19 @@ void glNamedFramebufferTexture(GLuint framebuffer, GLenum attachment, GLuint tex
             if (!fb->draw_attachment) {
                 fb->draw_attachment = new struct attachment_t[MAX_DRAW_BUFFERS];
             }
-            fb->draw_attachment[index]->textarget = textarget;
+            fb->draw_attachment[index].textarget = textarget;
             fb->draw_attachment[index].texture = texture;
             fb->draw_attachment[index].level = level;
         } else if (attachment == GL_DEPTH_ATTACHMENT) {
             if (!fb->read_attachment) fb->read_attachment = new attachment_t;
             fb->read_attachment->textarget = textarget;
-            fb->read_attachment[index].texture = texture;
-            fb->read_attachment[index].level = level;
+            fb->read_attachment[index]->texture = texture;
+            fb->read_attachment[index]->level = level;
         } else if (attachment == GL_STENCIL_ATTACHMENT) {
             if (!fb->read_attachment) fb->read_attachment = new attachment_t;
             fb->read_attachment->textarget = textarget;
-            fb->read_attachment[index].texture = texture;
-            fb->read_attachment[index].level = level;
+            fb->read_attachment[index]->texture = texture;
+            fb->read_attachment[index]->level = level;
         }
     }
 
