3736c718dac4b0b91a6a7a32d500771e086ce95b
aaaapai <89151134+aaaapai@users.noreply.github.com>
2025-07-25 00:54:53 +0800

    Update gles.h

--- src/main/cpp/gles/gles.h
+++ src/main/cpp/gles/gles.h
@@ -17,7 +17,6 @@ extern "C" {
 #endif
 
 GL_FUNC_TYPEDEF(void, glMultiDrawElementsEXT, GLenum mode, const GLsizei *count, GLenum type, const void *const*indices, GLsizei drawcount)
-GL_FUNC_TYPEDEF(void, glMultiDrawElementsBaseVertexEXT, GLenum mode, const GLsizei *count, GLenum type, const void *const*indices, GLsizei drawcount, const GLint *basevertex)
 GL_FUNC_TYPEDEF(void, glMultiDrawElementsBaseVertexOES, GLenum mode, const GLsizei *count, GLenum type, const void *const*indices, GLsizei drawcount, const GLint *basevertex)
 GL_FUNC_TYPEDEF(void, glQueryCounterEXT, GLuint id, GLenum target)
 GL_FUNC_TYPEDEF(void, glActiveTexture, GLenum texture)
