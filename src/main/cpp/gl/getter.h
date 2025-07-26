833ba50a1c4fd359bd1b3063e034ae02c259a5ea
aaaapai <89151134+aaaapai@users.noreply.github.com>
2025-07-26 23:47:32 +0800

    Update getter.h

--- src/main/cpp/gl/getter.h
+++ src/main/cpp/gl/getter.h
@@ -3,8 +3,8 @@
 //
 
 #include "../includes.h"
-#include "GL/gl.h"
-#include "GL/glcorearb.h"
+#include <GL/gl.h>
+#include <GL/glcorearb.h>
 #include "log.h"
 #include "../gles/loader.h"
 #include "mg.h"
