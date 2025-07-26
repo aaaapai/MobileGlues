e5a70e2df5cb30c3bd37f319d63f92c0e629c1da
aaaapai <89151134+aaaapai@users.noreply.github.com>
2025-07-26 23:35:04 +0800

    Update buffer.cpp

--- src/main/cpp/gl/buffer.cpp
+++ src/main/cpp/gl/buffer.cpp
@@ -704,7 +704,7 @@ void glBindVertexArray(GLuint array) {
 
 extern "C" {
 GLAPI GLAPIENTRY void *glMapBufferARB(GLenum target, GLenum access) __attribute__((alias("glMapBuffer")));
-GLAPI GLAPIENTRY void *glBufferDataARB(GLenum target, GLenum access) __attribute__((alias("glBufferData")));
+GLAPI GLAPIENTRY void glBufferDataARB(GLenum target, GLsizeiptr size, const void* data, GLenum usage) __attribute__((alias("glBufferData")));
 GLAPI GLAPIENTRY GLboolean glUnmapBufferARB(GLenum target) __attribute__((alias("glUnmapBuffer")));
 GLAPI GLAPIENTRY void glBufferStorageARB(GLenum target, GLsizeiptr size, const void* data, GLbitfield flags) __attribute__((alias("glBufferStorage")));
 GLAPI GLAPIENTRY void glBindBufferARB(GLenum target, GLuint buffer) __attribute__((alias("glBindBuffer")));
