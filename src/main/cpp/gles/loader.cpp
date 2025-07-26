def9cd704928706842cbe15001956553345da04f
aaaapai <89151134+aaaapai@users.noreply.github.com>
2025-07-27 01:40:20 +0800

    Update loader.cpp

--- src/main/cpp/gles/loader.cpp
+++ src/main/cpp/gles/loader.cpp
@@ -215,8 +215,8 @@ void InitGLESCapabilities() {
 	AppendExtension("OpenGL44");
         AppendExtension("OpenGL45");
         AppendExtension("OpenGL46");
-        //AppendExtension("GL_EXT_direct_state_access");
-        //AppendExtension("GL_ARB_direct_state_access");
+        AppendExtension("GL_EXT_direct_state_access");
+        AppendExtension("GL_ARB_direct_state_access");
     }
 
     if (global_settings.ext_compute_shader) {
