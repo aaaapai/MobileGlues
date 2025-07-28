d02a85589815980210f4c7a8521eb2177901d3c1
aaaapai <89151134+aaaapai@users.noreply.github.com>
2025-07-28 23:32:51 +0800

    Update glsl_for_es.cpp

--- src/main/cpp/gl/glsl/glsl_for_es.cpp
+++ src/main/cpp/gl/glsl/glsl_for_es.cpp
@@ -978,7 +978,7 @@ std::string spirv_to_essl(std::vector<unsigned int> spirv, uint essl_version, in
 }
 
 static bool glslang_inited = false;
-std::string GLSLtoGLSLES_2(const char *glsl_code, GLenum glsl_type, uint essl_version, int& return_code) {
+std::string GLSLtoGLSLES_2(const char *glsl_code, GLenum glsl_type, uint essl_version/*, int& return_code*/) {
 	bool atomicCounterEmulated = false;
     std::string correct_glsl_str = preprocess_glsl(glsl_code, glsl_type/*, &atomicCounterEmulated*/);
     LOG_D("Firstly converted GLSL:\n%s", correct_glsl_str.c_str())
@@ -1020,7 +1020,7 @@ std::string GLSLtoGLSLES_2(const char *glsl_code, GLenum glsl_type, uint essl_ve
     return essl;
 }
 
-std::string GLSLtoGLSLES_1(const char *glsl_code, GLenum glsl_type, uint esversion, int& return_code) {
+std::string GLSLtoGLSLES_1(const char *glsl_code, GLenum glsl_type, uint esversion/*, int& return_code*/) {
 #if !defined(__APPLE__)
     LOG_W("Warning: use glsl optimizer to convert shader.")
     if (esversion < 320) esversion = 320;
