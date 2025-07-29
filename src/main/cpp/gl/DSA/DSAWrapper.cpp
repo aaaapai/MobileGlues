cdbedad59aa75d2c71686483c78f8a4498576d75
aaaapai <89151134+aaaapai@users.noreply.github.com>
2025-07-29 19:59:29 +0800

    Update DSAWrapper.cpp

--- src/main/cpp/gl/DSA/DSAWrapper.cpp
+++ src/main/cpp/gl/DSA/DSAWrapper.cpp
@@ -695,6 +695,16 @@ void glFramebufferTexture(GLenum target, GLenum attachment, GLuint texture, GLin
             attach[attachment - GL_COLOR_ATTACHMENT0].level = level;
         }
         bound_framebuffer->current_target = target;
+    } else {
+	struct attachment_t* attach = bound_framebuffer->read_attachment;
+        if (attach) {
+            // Record generic texture as 2D for now
+            attach[attachment - GL_COLOR_ATTACHMENT0].textarget = texture ? GL_TEXTURE_2D : GL_NONE;
+            attach[attachment - GL_COLOR_ATTACHMENT0].texture = texture;
+            attach[attachment - GL_COLOR_ATTACHMENT0].level = level;
+        }
+        bound_framebuffer->current_target = target;
+ 
     }
     GLES.glFramebufferTexture(target, attachment, texture, level);
     CHECK_GL_ERROR
@@ -711,17 +721,23 @@ void glNamedFramebufferTexture(GLuint framebuffer, GLenum attachment, GLuint tex
 	temporarilyBindFramebuffer(framebuffer);
 
 	if (bound_framebuffer && attachment - GL_COLOR_ATTACHMENT0 < getMaxDrawBuffers()) {
-        struct attachment_t* attach =
-            (target == GL_DRAW_FRAMEBUFFER)
-                ? bound_framebuffer->draw_attachment
-                : bound_framebuffer->read_attachment;
-        if (attach) {
+          struct attachment_t* attach = bound_framebuffer->draw_attachment;
+          if (attach) {
             // Record generic texture as 2D for now
             attach[attachment - GL_COLOR_ATTACHMENT0].textarget = texture ? GL_TEXTURE_2D : GL_NONE;
             attach[attachment - GL_COLOR_ATTACHMENT0].texture = texture;
             attach[attachment - GL_COLOR_ATTACHMENT0].level = level;
-        }
-        bound_framebuffer->current_target = target;
+          }
+          bound_framebuffer->current_target = target;
+	} else {
+	    struct attachment_t* attach = bound_framebuffer->read_attachment;
+	    if (attach) {
+               // Record generic texture as 2D for now
+               attach[attachment - GL_COLOR_ATTACHMENT0].textarget = texture ? GL_TEXTURE_2D : GL_NONE;
+               attach[attachment - GL_COLOR_ATTACHMENT0].texture = texture;
+               attach[attachment - GL_COLOR_ATTACHMENT0].level = level;
+	    }
+	    bound_framebuffer->current_target = target;
 	}
 
 	glFramebufferTexture2D(GL_DRAW_FRAMEBUFFER, attachment, GL_TEXTURE_2D, texture, level);
