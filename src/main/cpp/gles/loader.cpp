b6862ba631790f2a3090e51da532337b96b8f221
aaaapai <89151134+aaaapai@users.noreply.github.com>
2025-07-30 20:00:50 +0800

    Update loader.cpp

--- src/main/cpp/gles/loader.cpp
+++ src/main/cpp/gles/loader.cpp
@@ -204,13 +204,6 @@ void InitGLESCapabilities() {
         AppendExtension("GL_EXT_timer_query");
     }
 
-    if (global_settings.ext_gl43) {
-	AppendExtension("OpenGL41");
-        AppendExtension("OpenGL42");
-        AppendExtension("OpenGL43");
-
-    }
-
     if (global_settings.ext_compute_shader) {
         AppendExtension("GL_ARB_compute_shader");
     }
@@ -220,12 +213,8 @@ void InitGLESCapabilities() {
 		AppendExtension("GL_EXT_direct_state_access");
     }
 
-	int glVersion = GLVersion.toInt(2);
-    for (int ver = 32; ver <= glVersion; ++ver) {
-        if (global_settings.ext_gl43 && ver == 43)
-			continue;
-        if (ver > 33 && ver < 40)
-			continue;
+    int glVersion = GLVersion.toInt(2);
+    for (int ver = 10; ver <= glVersion; ++ver) {
 		LOG_D("Appending OpenGL extension for version %d", ver)
 		AppendExtension(("OpenGL" + std::to_string(ver)).c_str());
     }
