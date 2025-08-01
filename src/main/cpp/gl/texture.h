4c720505f14bbb2381c4a7169ec570434bd7ca53
aaaapai <89151134+aaaapai@users.noreply.github.com>
2025-08-01 23:49:03 +0800

    Update texture.h

--- src/main/cpp/gl/texture.h
+++ src/main/cpp/gl/texture.h
@@ -17,8 +17,6 @@ void internal_convert(GLenum* internal_format, GLenum* type, GLenum* format);
 int nlevel(int size, int level);
 static int is_depth_format(GLenum format);
 static GLenum get_binding_for_target(GLenum target);
-std::shared_ptr<TextureObject> mgGetTexObjectByTarget(GLenum target);
-std::shared_ptr<TextureObject> mgGetTexObjectByID(unsigned texture);
 
 GLAPI GLAPIENTRY void glGenTextures( GLsizei n, GLuint *textures );
 GLAPI GLAPIENTRY void glTexParameterf(GLenum target, GLenum pname, GLfloat param);
