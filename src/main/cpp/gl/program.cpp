246f4fe05f5ace509ed0471097785c5b0d429e79
aaaapai <89151134+aaaapai@users.noreply.github.com>
2025-08-03 17:50:25 +0800

    Update program.cpp

--- src/main/cpp/gl/program.cpp
+++ src/main/cpp/gl/program.cpp
@@ -24,7 +24,7 @@ unordered_map<GLuint, bool> program_map_is_sampler_buffer_emulated;
 extern unordered_map<GLuint, bool> shader_map_is_atomic_counter_emulated;
 unordered_map<GLuint, bool> program_map_is_atomic_counter_emulated;
 
-char* updateLayoutLocation(const char* esslSource, GLuint color, const char* name) {
+static char* updateLayoutLocation(const char* esslSource, GLuint color, const char* name) {
     std::string shaderCode(esslSource);
 
     std::string pattern = std::string(R"((layout\s*$[^)]*location\s*=\s*\d+[^)]*$\s*)?)")
