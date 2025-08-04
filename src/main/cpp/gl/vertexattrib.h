395986809efbff3e76e6da7f76bae4f96143dbaa
aaaapai <89151134+aaaapai@users.noreply.github.com>
2025-08-04 19:46:44 +0800

    Update vertexattrib.h

--- src/main/cpp/gl/vertexattrib.h
+++ src/main/cpp/gl/vertexattrib.h
@@ -60,15 +60,8 @@ GLAPI GLAPIENTRY void glVertexAttribFormat(GLuint attribindex, GLint size, GLenu
                          GLboolean normalized, GLuint relativeoffset);
 GLAPI GLAPIENTRY void glVertexAttribIFormat(GLuint attribindex, GLint size, GLenum type,
                           GLuint relativeoffset);
-GLAPI GLAPIENTRY void glVertexArrayAttribFormat(GLuint vaobj, GLuint attribindex, GLint size,
-                              GLenum type, GLboolean normalized,
-                              GLuint relativeoffset);
-GLAPI GLAPIENTRY void glVertexArrayAttribIFormat(GLuint vaobj, GLuint attribindex, GLint size,
-                               GLenum type, GLuint relativeoffset);
 GLAPI GLAPIENTRY void glVertexAttribLFormat(GLuint attribindex, GLint size, GLenum type,
                           GLuint relativeoffset);
-GLAPI GLAPIENTRY void glVertexArrayAttribLFormat(GLuint vaobj, GLuint attribindex, GLint size,
-                               GLenum type, GLuint relativeoffset);
 
 #ifdef __cplusplus
 }
