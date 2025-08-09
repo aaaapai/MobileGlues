ca5b3366b320dc81a16c86d62ecd7d5c2edfcb23
aaaapai <89151134+aaaapai@users.noreply.github.com>
2025-08-09 18:49:01 +0800

    Update loader.cpp

--- src/main/cpp/egl/loader.cpp
+++ src/main/cpp/egl/loader.cpp
@@ -48,7 +48,7 @@ void init_target_egl(void) {
             EGL_ALPHA_SIZE, 8,
             EGL_DEPTH_SIZE, 24,
             EGL_SURFACE_TYPE, EGL_WINDOW_BIT|EGL_PBUFFER_BIT,
-            EGL_RENDERABLE_TYPE, EGL_OPENGL_ES3_BIT_KHR,
+            EGL_RENDERABLE_TYPE, EGL_OPENGL_ES3_BIT,
             EGL_NONE
     };
 
@@ -71,11 +71,11 @@ void init_target_egl(void) {
         goto cleanup;
     }
 
-    if (global_settings.angle == AngleMode::Enabled) {
+    /*if (global_settings.angle == AngleMode::Enabled) {
         BindAPI_result = egl_eglBindAPI(EGL_OPENGL_API);
-    } else {
+    } else {*/
         BindAPI_result = egl_eglBindAPI(EGL_OPENGL_ES_API);
-    }
+    //}
     if (BindAPI_result != EGL_TRUE) {
         LOG_E("eglBindAPI failed (0x%x)", egl_eglGetError());
         goto cleanup;
