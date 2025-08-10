33054b6a53e9d134cfc4f6fda889ba5e299f725a
aaaapai <89151134+aaaapai@users.noreply.github.com>
2025-08-10 14:30:08 +0800

    Update framebuffer.cpp

--- src/main/cpp/gl/framebuffer.cpp
+++ src/main/cpp/gl/framebuffer.cpp
@@ -104,7 +104,18 @@ void glFramebufferTexture2D(GLenum target, GLenum attachment, GLenum textarget, 
 
     LOG_D("glFramebufferTexture2D(0x%x, 0x%x, 0x%x, %d, %d)", target, attachment, textarget, texture, level)
 
-    struct attachment_t* attach = nullptr;
+    if (target == GL_FRAMEBUFFER) {
+        glFramebufferTexture2D(GL_DRAW_FRAMEBUFFER, attachment, textarget, texture, level);
+        glFramebufferTexture2D(GL_READ_FRAMEBUFFER, attachment, textarget, texture, level);
+        return;
+    }
+
+    //struct attachment_t* attach = nullptr;
+    struct attachment_t* attach =
+            (target == GL_DRAW_FRAMEBUFFER)
+                ? bound_framebuffer->draw_attachment
+                : bound_framebuffer->read_attachment;
+    
     if(texture == 0) {
         attach[attachment - GL_COLOR_ATTACHMENT0].textarget = GL_NONE;
         rebind_framebuffer(target, attachment);
@@ -112,10 +123,6 @@ void glFramebufferTexture2D(GLenum target, GLenum attachment, GLenum textarget, 
     }
 
     if (bound_framebuffer && attachment - GL_COLOR_ATTACHMENT0 <= static_cast<GLuint>(getMaxDrawBuffers())) {
-        if (target == GL_DRAW_FRAMEBUFFER)
-            attach = bound_framebuffer->draw_attachment;
-        else
-            attach = bound_framebuffer->read_attachment;
 
         if (attach) {
             attach[attachment - GL_COLOR_ATTACHMENT0].textarget = textarget;
@@ -125,38 +132,6 @@ void glFramebufferTexture2D(GLenum target, GLenum attachment, GLenum textarget, 
 
         bound_framebuffer->current_target = target;
     }
-
-    /*if (target == GL_FRAMEBUFFER) {
-        target = GL_DRAW_FRAMEBUFFER;
-    }
-
-    if (bound_framebuffer) {
-        struct attachment_t* attach =
-            (target == GL_DRAW_FRAMEBUFFER)
-                ? bound_framebuffer->draw_attachment
-                : bound_framebuffer->read_attachment;
-                
-        if (attach) {
-            GLuint attach_index = 0;
-            if (attachment >= GL_COLOR_ATTACHMENT0 && attachment <= GL_COLOR_ATTACHMENT0 + getMaxDrawBuffers()) {
-                attach_index = attachment - GL_COLOR_ATTACHMENT0;
-            } else if (attachment == GL_DEPTH_ATTACHMENT) {
-                attach_index = getMaxDrawBuffers();
-            } else if (attachment == GL_STENCIL_ATTACHMENT) {
-                attach_index = getMaxDrawBuffers() + 1;
-            } else if (attachment == GL_DEPTH_STENCIL_ATTACHMENT) {
-                // Handle combined depth/stencil
-                attach_index = getMaxDrawBuffers();
-                bound_framebuffer->draw_attachment[getMaxDrawBuffers() + 1].texture = texture;
-                bound_framebuffer->draw_attachment[getMaxDrawBuffers() + 1].level = level;
-            }
-            
-            attach[attach_index].textarget = GL_TEXTURE_2D; // For ES, we might not have other types
-            attach[attach_index].texture = texture;
-            attach[attach_index].level = level;
-        }
-        bound_framebuffer->current_target = target;
-    }*/
 
     GLES.glFramebufferTexture2D(target, attachment, textarget, texture, level);
 
