d6d34a7d1e93e37d4a158f141cbc7601aa43ca00
aaaapai <89151134+aaaapai@users.noreply.github.com>
2025-08-22 00:29:38 +0800

    Update glsl_for_es.h

--- src/main/cpp/gl/glsl/glsl_for_es.h
+++ src/main/cpp/gl/glsl/glsl_for_es.h
@@ -17,6 +17,7 @@ extern "C" {
 }
 #endif
 
+std::string getCachedESSL(const char *glsl_code, uint essl_version);
 std::string GLSLtoGLSLES(const char *glsl_code, GLenum glsl_type,
                          uint essl_version, uint glsl_version,
                          int &return_code);
@@ -25,5 +26,7 @@ std::string GLSLtoGLSLES_1(const char *glsl_code, GLenum glsl_type,
 std::string GLSLtoGLSLES_2(const char *glsl_code, GLenum glsl_type,
                            uint essl_version, int &return_code);
 int getGLSLVersion(const char *glsl_code);
+
+extern std::string MesaConvertShader (const char *src, GLenum type, unsigned int glsl, unsigned int essl);
 
 #endif
