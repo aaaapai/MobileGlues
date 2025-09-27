39b0b70f55f018a054884ad442455609f91f7f4c
aaaapai <89151134+aaaapai@users.noreply.github.com>
2025-09-27 08:47:49 +0800

    Update multidraw.h

--- src/main/cpp/gl/multidraw.h
+++ src/main/cpp/gl/multidraw.h
@@ -16,6 +16,14 @@
 #include "../gles/loader.h"
 #include "mg.h"
 
+#include <vector>
+#include <algorithm>
+#include <mutex>
+#include <cmath>
+#include <cassert>
+#include <arm_neon.h>
+#include <thread>
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -26,6 +34,7 @@ struct draw_elements_indirect_command_t {
     GLuint  firstIndex;
     GLint   baseVertex;
     GLuint  reservedMustBeZero;
+    GLuint  baseInstance;
 };
 
 struct drawcmd_compute_t {
@@ -46,6 +55,9 @@ GLAPI GLAPIENTRY void mg_glMultiDrawElements_multiindirect(GLenum mode, const GL
 GLAPI GLAPIENTRY void mg_glMultiDrawElements_basevertex(GLenum mode, const GLsizei *count, GLenum type, const void *const *indices, GLsizei primcount);
 GLAPI GLAPIENTRY void mg_glMultiDrawElements_drawelements(GLenum mode, const GLsizei *count, GLenum type, const void *const *indices, GLsizei primcount);
 GLAPI GLAPIENTRY void mg_glMultiDrawElements_compute(GLenum mode, const GLsizei *count, GLenum type, const void *const *indices, GLsizei primcount);
+
+GLAPI GLAPIENTRY void glMultiDrawElementsIndirect(GLenum mode, GLenum type, const void *indirect, GLsizei drawcount, GLsizei stride);
+GLAPI GLAPIENTRY void mg_glMultiDrawElementsIndirect_deepseek_one(GLenum mode, GLenum type, const void *indirect, GLsizei drawcount, GLsizei stride);
 
 #ifdef __cplusplus
 }
