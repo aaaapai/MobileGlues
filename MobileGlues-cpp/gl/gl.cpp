05edbbe16ecbbd0c2472debbdf2e693bf074da23
aaaapai <89151134+aaaapai@users.noreply.github.com>
2026-03-22 09:13:16 +0800

    Update gl.cpp

--- MobileGlues-cpp/gl/gl.cpp
+++ MobileGlues-cpp/gl/gl.cpp
@@ -119,23 +119,18 @@ void glClear(GLbitfield mask) {
     LOG_D("glClear, mask = 0x%x", mask);
 
     INIT_CHECK_GL_ERROR
-
     CHECK_GL_ERROR_NO_INIT
 
-    if (global_settings.angle == AngleMode::Enabled && mask == GL_DEPTH_BUFFER_BIT &&
-        std::fabs(currentDepthValue - 1.0) <= 0.001 && framebuffers[current_draw_fbo].color_attachments_all_none) {
-        LOG_D("doing depth workaround")
-        if (global_settings.angle_depth_clear_fix_mode == AngleDepthClearFixMode::Mode1)
-            // Workaround for ANGLE depth-clear bug: if depth≈1.0, draw a fullscreen triangle at z=1.0 to force actual
-            // depth buffer write.
-            DrawDepthClearTri();
-        else if (global_settings.angle_depth_clear_fix_mode == AngleDepthClearFixMode::Mode2) {
-            // Or just explicitly clear depth buffer and see what's happened
-            const GLfloat clear_depth_value = 1.0f;
-            GLES.glClearBufferfv(GL_DEPTH, 0, &clear_depth_value);
-        }
-        // Clear again
-        GLES.glClear(mask);
+    if (global_settings.angle == AngleMode::Enabled && 
+        mask == GL_DEPTH_BUFFER_BIT &&
+        std::fabs(currentDepthValue - 1.0) <= 0.001 && 
+        framebuffers[current_draw_fbo].color_attachments_all_none) {
+        
+        LOG_D("doing depth workaround (Invalidate + ClearBufferfv)");
+        GLenum attachments[] = {GL_DEPTH_ATTACHMENT};
+        GLES.glInvalidateFramebuffer(GL_FRAMEBUFFER, 1, attachments);
+        const GLfloat clear_depth_value = 1.0f;
+        GLES.glClearBufferfv(GL_DEPTH, 0, &clear_depth_value);
     } else {
         GLES.glClear(mask);
     }
