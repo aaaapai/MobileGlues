ec055a0349efdd847fdd602c418a80787505fc49
aaaapai <89151134+aaaapai@users.noreply.github.com>
2026-03-22 09:06:02 +0800

    Update gl.cpp

--- MobileGlues-cpp/gl/gl.cpp
+++ MobileGlues-cpp/gl/gl.cpp
@@ -24,6 +24,9 @@ extern std::vector<framebuffer_t> framebuffers;
 
 void glClearDepth(GLclampd depth) {
     LOG()
+    if (depth < 0.0) depth = 0.0;
+    if (depth > 1.0) depth = 1.0;
+    
     currentDepthValue = depth;
     GLES.glClearDepthf((float)depth);
     CHECK_GL_ERROR
@@ -120,7 +123,7 @@ void glClear(GLbitfield mask) {
     CHECK_GL_ERROR_NO_INIT
 
     if (global_settings.angle == AngleMode::Enabled && mask == GL_DEPTH_BUFFER_BIT &&
-        std::fabs(currentDepthValue - 1.0f) <= 0.001f && framebuffers[current_draw_fbo].color_attachments_all_none) {
+        std::fabs(currentDepthValue - 1.0) <= 0.001 && framebuffers[current_draw_fbo].color_attachments_all_none) {
         LOG_D("doing depth workaround")
         if (global_settings.angle_depth_clear_fix_mode == AngleDepthClearFixMode::Mode1)
             // Workaround for ANGLE depth-clear bug: if depth≈1.0, draw a fullscreen triangle at z=1.0 to force actual
