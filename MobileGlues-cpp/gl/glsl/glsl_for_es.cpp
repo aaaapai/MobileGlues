2fa9f76ad7b285f869800bbb35d6aff09e9258c4
aaaapai <89151134+aaaapai@users.noreply.github.com>
2026-08-05 17:50:47 +0800

    Update glsl_for_es.cpp

--- MobileGlues-cpp/gl/glsl/glsl_for_es.cpp
+++ MobileGlues-cpp/gl/glsl/glsl_for_es.cpp
@@ -22,7 +22,7 @@
 #include "cache.h"
 #include "../../version.h"
 
-#define DEBUG 1
+#define DEBUG 0
 
 const char* atomicCounterEmulatedWatermark = "// Non-opaque atomic uniform converted to SSBO";
 
